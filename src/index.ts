dabf1f3cc7dd6debf3410228d4f1e96d9b3f58fe
cnuebred <cnuebred@gmail.com>
2023-06-05 00:36:15 +0200

    add index
    prepare to npm package format

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import { CellAttributes } from "./attributes";
+import { Cell } from "./cell";
+import { Core } from "./core";
+import { CellLocation } from "./d";
+import { Form } from "./modules/form";
+import { Table } from "./modules/table";
+import { CellReplacements } from "./replace";
+import { CellStyle } from "./style";
+import { CellText } from "./text";
+import { CellRenderOptionsDefault } from "./utils";
+import { CellWorker } from "./worker";
+
+
+
+export {
+    Cell,
+    Core,
+    CellReplacements,
+    CellLocation,
+    CellAttributes,
+    CellStyle,
+    CellText,
+    CellRenderOptionsDefault,
+    CellWorker,
+    Form as CarbeeForm,
+    Table as CarbeeTable
+}
