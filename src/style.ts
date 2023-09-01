a5e85919d77bf68ed3f33901a239905f856ca07c
Cube <46578659+cnuebred@users.noreply.github.com>
2023-09-01 22:58:05 +0000

    refactor style module -v1

--- src/style.ts
+++ src/style.ts
@@ -1,7 +1,5 @@
-import { readFile } from "fs";
-import { CSSList, CssObject, QueryType } from "./d";
+import { CssObject, QueryType } from "./d";
 import { CellTree } from "./utils";
-import { Cell } from "./cell";
 import path from "path";
 
 
@@ -9,75 +7,97 @@ const change_to_css_style = (key) => {
     return key.replaceAll(/([a-z])([A-Z])/gm, '$1-$2').toLowerCase()
 }
 
-class CellStyleEntity {
-    query: QueryType
-    style: CssObject = {}
-    constructor(query: QueryType, style: CssObject) {
-        this.query = query
-        this.style = style
-    }
-    render() {
-        const list = Object.entries(this.style).map(([key, value]) => {
-            return `${change_to_css_style(key)}: ${value}`
-        })
-        return `${this.query}{${list.join(';')}}`
-    }
-}
+// export const style_entity = (style: StyleObject) => ({
+//     map: (fn: (style: StyleObject) => StyleObject) => style_entity(fn(style)),
+//     render: () => `${style.query}{${Object.entries(style.css).map(([key, value]) => {
+//         return `${change_to_css_style(key)}: ${value}`
+//     }).join(';')}}`
+// })
 
-type StyleObject = {
-    query?: QueryType,
-    style: CssObject
-}
+// type StyleEntity = ReturnType<typeof style_entity>
+
+export type StyleObject = CssObject & { imports?: string[], query?: QueryType, children?: StyleObject[] }
+const STYLE_OBJ_FUNCTORS = ['imports', 'query', 'children']
+
 
 export class CellStyle extends CellTree {
-    readonly imports: { source: string, url: boolean }[] = []
-    styles: CellStyleEntity[] = []
-    classes: string[] = []
-    constructor() { super() }
+    imports_list: { source: string, render: string, url: boolean }[] = []
+    // entities: StyleObject[] = []
+    css: StyleObject = {}
+    constructor(query?: string, css?: StyleObject) {
+        super()
+        if (query) this.query = query
+        if (css) this.from(css)
+    }
     empty(): boolean {
-        return this.styles.length == 0 && this.import.length == 0
+        for (let key in this.css) {
+            if (this.css.hasOwnProperty(key)) {
+                return false;
+            }
+        }
+        return true;
     }
-    join() {
-        return this.styles.map(item => { return item.render() }).join(';')
-    }
-    generate(): Cell {
-        const script = new Cell('style')
-        script.text(this.join())
-        return script
-    }
-    import(value: string): CellTree {
-        const is_url = value.match(/http|https/gm)
-        if (!is_url)
-            value = path.resolve(value)
+    push_children(...csss: StyleObject[]) {
+        if (this.css.children)
+            this.css.children.push(...csss)
         else
-            value = `url("${value}")`
-        this.imports.push({ source: value, url: !!is_url })
+            this.css.children = csss
+    }
+    parser(query: string, csss: StyleObject[]): string {
+        let single_css = ''
+
+        csss.forEach(css => {
+            const local_query = css.query ? `${query}${css.query.startsWith(':') ? '' : ' '}${css.query}` : query
+            const style_properties = Object.entries(css)
+                .map(([key, value]) => {
+                    if (STYLE_OBJ_FUNCTORS.includes(key)) return null
+                    return `${change_to_css_style(key)}:${value};`
+                })
+                .filter(item => !!item).join('')
+            single_css += `${local_query} {${style_properties}} `
+            if (css.children) single_css += this.parser(local_query, css.children)
+        })
+
+        return single_css
+    }
+    render(): string {
+        return this.parser(this.query, [this.css])
+    }
+    // join() {
+    //     return this.entities.map(item => { return style_entity(item).render() }).join(';')
+    // }
+    // generate(): Cell {
+    //     const script = new Cell('style')
+    //     script.text(this.join())
+    //     return script
+    // }
+    push_import(value: string): CellTree {
+        const is_url = value.match(/http|https/gm)
+        let render_value: string = ''
+        if (!is_url)
+            render_value = path.resolve(value)
+        else
+            render_value = `url("${value}")`
+        console.log(value)
+        this.imports_list.push({ source: value, render: render_value, url: !!is_url })
         return this
     }
-    add(styleObject: StyleObject) {
-        const style_entity = new CellStyleEntity(`${this.query} ${styleObject.query || ''}`, styleObject.style)
-        this.styles.push(style_entity)
-    }
-    from(style: object) {
-        if (style['import']) {
-            style['import'].forEach(item => {
-                this.import(item)
-            })
-            delete style['import']
-        }
-        this.add({ style })
-    }
-    class(...names: string[]) {
-        names.forEach(name => {
-            this.owner.attributes.append('class', name)
-            this.classes.push(name)
-        })
+    // #from_node(style: StyleObject) {
+    //     style.imports.forEach(item => this.import(item))
+
+    //     style.query = style.query ? style.query.replace(BASE_QUERY_STYLE_SELECTOR_REPLACE, this.query) : style.query = `${this.query}${style.query}`
+
+    //     this.entities.push(style)
+    // }
+    from(style: StyleObject) {
+        if(style) this.css = { ...style }
+        if (style && style.imports) style.imports.forEach(item => this.push_import(item))
+
     }
     copy(): CellStyle {
         const style_copy = new CellStyle()
-        style_copy.styles = [...this.styles]
-        style_copy.classes = [...this.classes]
-
+        style_copy.css = JSON.parse(JSON.stringify(this.css))
+        style_copy.imports_list = [...this.imports_list]
         return style_copy
     }
 }
---
+++ test/style.test.ts
@@ -0,0 +1,78 @@
+import path from 'path';
+import { CellStyle, StyleObject } from '../src/style';
+
+describe('Style module tests - basic', () => {
+    let style: CellStyle
+    const style_css: StyleObject = {
+        color: 'red',
+        top: '30%',
+        children: [
+            {
+                query: 'p',
+                color: 'blue'
+            },
+            {
+                query: ':hover',
+                opacity: 0.3
+            }
+        ]
+    }
+    beforeAll(() => {
+        style = new CellStyle('div.foo', style_css)
+    })
+    test('init basic style', () => {
+        expect(style.css).toStrictEqual(style_css)
+    })
+    test('empty style condition', () => {
+        const style = new CellStyle('div.foo', {})
+        expect(style.empty()).toStrictEqual(true)
+    })
+    test('render style to css', () => {
+        expect(style.render()).toBe(`div.foo {color:red;top:30%;} div.foo p {color:blue;} div.foo:hover {opacity:0.3;} `)
+    })
+    test('push child to object', () => {
+        const third_child = {
+            query: 'span',
+            color: 'black'
+        }
+        style.push_children(third_child)
+        expect(style.css.children[2]).toStrictEqual(third_child)
+    })
+})
+
+describe('Style module tests - imports', () => {
+    let style: CellStyle
+    const style_css:StyleObject = {
+        imports: ['https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css'],
+        color: 'red',
+        top: '30%'
+    }
+    beforeAll(() => {
+        style = new CellStyle()
+        style.from(style_css)
+    })
+    test('init imports', () => {
+        expect(style.imports_list).toStrictEqual([
+            {
+                source: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css',
+                render: 'url(\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css\")',
+                url: true
+            }
+        ])
+    })
+    test('add imports', () => {
+        style.push_import('./style.css')
+        expect(style.imports_list).toStrictEqual([
+            {
+                source: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css',
+                render: 'url(\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css\")',
+                url: true
+            },
+            {
+                source: './style.css',
+                render: path.resolve('./style.css'),
+                url: false
+            }
+        ])
+    })
+})
