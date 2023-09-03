df8a8b48e3861004bad16abd5791abac3942e657
Cube <46578659+cnuebred@users.noreply.github.com>
2023-09-03 00:46:55 +0000

    update imports in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
 import { CellAttributes } from "./attributes";
 import { Cell } from "./cell";
 import { Core } from "./core";
-import { CellLocation } from "./d";
+import { CellLocation, CallbackCog, StyleObject, CssObject } from "./d";
 import { Form } from "./modules/form";
 import { Table } from "./modules/table";
 import { CellReplacements } from "./replace";
 import { CellStyle } from "./style";
-import { txt, TxtType } from "./text";
 import { CELL_RENDER_OPTIONS_DEFAULT } from "./utils";
 import { CellWorker } from "./worker";
 
@@ -19,10 +18,11 @@ export {
     CellLocation,
     CellAttributes,
     CellStyle,
-    txt,
-    TxtType,
     CELL_RENDER_OPTIONS_DEFAULT,
     CellWorker,
+    CallbackCog,
+    StyleObject,
+    CssObject,
     Form as HivecraftForm,
     Table as HivecraftTable
 }
